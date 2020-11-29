bbd8b1ff92c8996da42e7b27d1bb7161ae2ae107
Ayesha Asif <ayaseef@gmail.com>
2020-11-28 21:10:00 -0800

    Added uid and provider migration

--- db/migrate/20201129050606_add_uid_and_provider_to_users.rb
+++ db/migrate/20201129050606_add_uid_and_provider_to_users.rb
@@ -3,6 +3,5 @@ class AddUidAndProviderToUsers < ActiveRecord::Migration[6.0]
     add_column :users, :uid, :integer
     add_column :users, :provider, :string
     add_column :users, :email, :string
-    add_column :users, :name, :string
   end
 end
