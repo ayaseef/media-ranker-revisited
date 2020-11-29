7e25600c513d89a2c020a49e1c5eac95e894519b
Ayesha Asif <ayaseef@gmail.com>
2020-11-28 20:50:03 -0800

    Added user logging out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
-
 class UsersController < ApplicationController
+  skip_before_action :find_user, only: [:create]
+
   def index
     @users = User.all
   end
@@ -9,53 +10,31 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
-  def login_form
-  end
-
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
-      end
-    end
-    redirect_to root_path
-  end
-
   def create
     auth_hash = request.env["omniauth.auth"]
 
     user = User.find_by(uid: auth_hash[:uid], provider: "github")
     if user
       # User was found in the database
-      flash[:success] = "Logged in as returning user #{user.username}"
+      flash[:status] = :success
+      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
     else
       # User doesn't match anything in the DB
       # Attempt to create a new user
       user = User.build_from_github(auth_hash)
 
       if user.save
-        flash[:success] = "Logged in as new user #{user.username}"
+        flash[:status] = :success
+        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
       else
         # Couldn't save the user for some reason. If we
         # hit this it probably means there's a bug with the
         # way we've configured GitHub. Our strategy will
         # be to display error messages to make future
         # debugging easier.
-        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        flash[:status] = :failure
+        flash[:result_text] = "Could not log in"
+        flash[:messages] = user.errors.messages
         return redirect_to root_path
       end
     end
@@ -67,15 +46,8 @@ class UsersController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "Successfully logged out!"
-
-    redirect_to root_path
-  end
-
-  def logout
-    session[:user_id] = nil
     flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
+    flash[:result_text] = 'Successfully logged out.'
     redirect_to root_path
   end
 end
